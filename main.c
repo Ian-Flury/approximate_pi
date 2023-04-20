5dd7793023eca5328823794e12b5b9d249136b7a
ianfl <ianflury123@gmail.com>
2023-04-20 10:37:19 -0700

    clean up before adding threading.

--- main.c
+++ main.c
@@ -2,38 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
 #include <pthread.h>
 
 double calcLoop(int iterations);
 
-
 int main() {
-    clock_t start, end;
+    double numInCircle;
     double cpu_time_used;
+    int iterations = 100000000;
 
-    // Seed the random number generator with the current time
+    clock_t start, end;
     srand(time(NULL));
 
     pthread_t id_1;
     pthread_t id_2;
 
-
-    double numInCircle = 0;
-
-    unsigned long int allPoints = 4000000000;
-
     start = clock();
 
-
+    // iteration function
+    numInCircle = calcLoop(iterations);
 
     end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-    double pi = 4 * (numInCircle / allPoints);
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    double pi = 4 * (numInCircle / iterations);
     printf("Approximation of Pi: %f\n\n", pi);
-    printf("iterations: %u\n", allPoints);
-    printf("CPU time used: %.0f secs\n", cpu_time_used);
+    printf("iterations: %u\n", iterations);
+    printf("CPU time used: %f secs\n", cpu_time_used);
 
     return 0;
 }
@@ -42,7 +37,7 @@ double calcLoop(int iterations) {
     double x;
     double y;
     double distance = 0.0;
-    double numInCircle;
+    double numInCircle = 0.0;
     for (int i = 0; i < iterations; i++) {
         x = (double) rand() / RAND_MAX;
         y = (double) rand() / RAND_MAX;
