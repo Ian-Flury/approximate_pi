f93ab182a3601b347a9d1e837bc3c2762e6bad4d
ianfl <ianflury123@gmail.com>
2023-04-21 16:30:46 -0700

    Added multi-threading to speed up approximation. Runs on 16 threads.

--- main.c
+++ main.c
@@ -4,41 +4,78 @@
 #include <math.h>
 #include <pthread.h>
 
-double calcLoop(int iterations);
+#define NUM_OF_THREADS 16
+#define TOTAL_ITERATIONS 100000000
+
+void *calcLoop(void *params);
+
+// will be used to pass parameters to threads
+typedef struct {
+    double retVal;
+    int numIt;
+} parameters;
 
 int main() {
-    double numInCircle;
     double cpu_time_used;
-    int iterations = 100000000;
-
+    int iterationsPerThread = TOTAL_ITERATIONS / NUM_OF_THREADS;
     clock_t start, end;
     srand(time(NULL));
 
-    pthread_t id_1;
-    pthread_t id_2;
+    pthread_t workers[NUM_OF_THREADS];
+
+    parameters *params;
+    params = (parameters*) malloc(NUM_OF_THREADS * sizeof (parameters));
+
+    // set the num of iterationsPerThread for each thread in their structs
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        (params+i)->numIt = iterationsPerThread;
+    }
 
     start = clock();
 
-    // iteration function
-    numInCircle = calcLoop(iterations);
+    // create NUM_OF_THREADS threads
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        pthread_create(&workers[i], NULL, calcLoop, (params+i));
+    }
+    // threads will automatically join when they're done
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        pthread_join(workers[i], NULL);
+    }
 
+    // performance monitoring
     end = clock();
-
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    double pi = 4 * (numInCircle / iterations);
+
+    // sum each of the thread totals
+    double totalInCircle = 0.0;
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        totalInCircle += (params+i)->retVal;
+    }
+
+    double pi = 4 * (totalInCircle / TOTAL_ITERATIONS);
     printf("Approximation of Pi: %f\n\n", pi);
-    printf("iterations: %u\n", iterations);
+    printf("Total Iterations: %u\n", TOTAL_ITERATIONS);
+    printf("Number of threads: %d\n", NUM_OF_THREADS);
+    printf("iterationsPerThread: %u\n", iterationsPerThread);
     printf("CPU time used: %f secs\n", cpu_time_used);
 
+    // don't forget to free malloced memory
+    free(params);
     return 0;
 }
 
-double calcLoop(int iterations) {
+// thread function to approximate pi
+// Throw darts at a box that has a circle in it.
+// The ratio of the darts in to the darts out is
+// approximately pi. Only do quadrant 2 and then
+// multiply by 4 at the end.
+void *calcLoop(void *params) {
+    parameters *p = (parameters *)params;
     double x;
     double y;
     double distance = 0.0;
     double numInCircle = 0.0;
-    for (int i = 0; i < iterations; i++) {
+    for (int i = 0; i < p->numIt; i++) {
         x = (double) rand() / RAND_MAX;
         y = (double) rand() / RAND_MAX;
         distance = sqrt((x*x) + (y*y));
@@ -46,5 +83,6 @@ double calcLoop(int iterations) {
             numInCircle++;
         }
     }
-    return numInCircle;
+    p->retVal = numInCircle;
+    pthread_exit(0);
 }
